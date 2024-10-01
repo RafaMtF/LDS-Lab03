5c9bd0d3d82fdd0d43bd47a373658adfa75dcfd7
RafaMtF <1452876@sga.pucminas.br>
2024-10-01 10:03:00 -0300

    Adicionando configurações iniciais ao projeto

---
+++ backend/src/main/java/com/lab/moeda_estudantil/MoedaEstudantilApplication.java
@@ -0,0 +1,13 @@
+package com.lab.moeda_estudantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoedaEstudantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoedaEstudantilApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lab/moeda_estudantil/MoedaEstudantilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.moeda_estudantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoedaEstudantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
